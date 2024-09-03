36bc4fca36441585d969cd5afabefe56a5f935fe
macadmin <macadmin@HWPO-V2.local>
2024-09-03 13:39:37 +0700

    print changes made  commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is the first changes on GitDemo");
+        System.out.println("This is the second changes on GitDemo 2");
+        System.out.println("This is the second changes on GitDemo 3");
     }
 }
